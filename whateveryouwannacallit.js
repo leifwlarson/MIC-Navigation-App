1e4c0add80b56ec3549be6785d75bc5255d07939
tgalvez712 <tgalvez712@lsr7sd.org>
2018-05-17 13:10:46 -0500

    Add updated JS and HTML for map boi

--- whateveryouwannacallit.js
+++ whateveryouwannacallit.js
@@ -1,6 +1,6 @@
 function myCanvas() 
 {
-    var c = document.getElementById("myCanvas");
+    var c = document.createElement("canvas");
     var ctx = c.getContext("2d");
     var img = document.getElementById("main_map");
     ctx.drawImage(img,0,0);
